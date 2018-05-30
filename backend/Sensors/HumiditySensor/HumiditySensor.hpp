66797fc5758f22f1c9bffbf002595f195e3d2566
Nils <nils.kirchhof@t-online.de>
2018-05-30 16:00:33 +0200

    Objekte zu Sensoren erstellt, Feuchtigkeit und UV Sensor brauchen ADC, noch nicht mit Qt getestet

---
+++ backend/Sensors/HumiditySensor/HumiditySensor.hpp
@@ -0,0 +1,42 @@
+#ifndef HUMIDITYSENSOR_H
+#define HUMIDITYSENSOR_H
+
+#include "./../Sensor.h"
+#include "./../Bcm2835Interface.hpp"
+#include <sys/time.h>
+#include <time.h>
+
+#define BASIS_ADDR              (0x51)
+#define SENSITIVITY_ADDR_LOW    (0x0A)
+#define SENSITIVITY_ADDR_HIGH   (0x0B)
+#define OFFSET_ADDR_LOW         (0x0C)
+#define OFFSET_ADDR_HIGH        (0x0D)
+
+
+
+
+class HumiditySensor : Sensor{
+
+    public:
+    HumiditySensor(uint8_t pin, uint32_t sensorID, Bcm2835Interface *wire);
+    ~HumiditySensor();
+
+    void enableAutoRange(bool enabled);
+    bool getEvent(sensors_event_t*);
+    void getSensor(sensor_t*);
+    float calculateHumidity();
+
+    private:
+    void calibrate();
+    float readFrequenz();
+
+
+    Bcm2835Interface* _wire;
+    uint8_t _pin;
+    uint32_t _sensorID;
+    uint16_t _offset = 0;
+    uint16_t _sensitivity = 0;
+
+};
+
+#endif
