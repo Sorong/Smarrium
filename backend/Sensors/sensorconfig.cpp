cbce279061328fb697f72eeca2f00fd4d6f1575e
Nils <nils.kirchhof@t-online.de>
2018-07-16 12:05:51 +0200

    g

--- backend/Sensors/sensorconfig.cpp
+++ backend/Sensors/sensorconfig.cpp
@@ -6,16 +6,10 @@ SensorConfig::SensorConfig(QJsonObject json) : QJsonObject(json)
 
 float SensorConfig::getMinValue()
 {
-    int currentHour = QTime::currentTime().hour();
-    QJsonArray configList = this->operator []("config");
-    float min = (float) configList[currentHour][currentHour+""]["min"].toDouble();
-    return min;
+
 }
 
 float SensorConfig::getMaxValue()
 {
-    int currentHour = QTime::currentTime().hour();
-    QJsonArray configList = this->operator []("config");
-    float max = (float) configList[currentHour][currentHour+""]["max"].toDouble();
-    return max;
+
 }
