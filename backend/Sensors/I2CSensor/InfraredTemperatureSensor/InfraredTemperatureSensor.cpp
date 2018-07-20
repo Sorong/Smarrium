0d07d09ee6d4b32d41a7a4e8e0100b057fa9f378
Nils <nils.kirchhof@t-online.de>
2018-07-20 17:47:17 +0200

    Lizenzen

--- backend/Sensors/I2CSensor/InfraredTemperatureSensor/InfraredTemperatureSensor.cpp
+++ backend/Sensors/I2CSensor/InfraredTemperatureSensor/InfraredTemperatureSensor.cpp
@@ -1,3 +1,18 @@
+/******************************************************************************
+SparkFunMLX90614.cpp
+Source file for the SparkFun IR Thermometer Library
+Jim Lindblom @ SparkFun Electronics
+October 23, 2015
+https://github.com/sparkfun/SparkFun_MLX90614_Arduino_Library
+This file defines the SMBus hardware interface(s) for the MLX90614 IR thermometer
+and abstracts temperature measurments and other features of the MLX90614
+Development environment specifics:
+Arduino 1.6.5
+SparkFun IR Thermometer Evaluation Board - MLX90614
+******************************************************************************/
+
+
+
 #include "InfraredTemperatureSensor.hpp"
 
 IRTemperatureSensor::IRTemperatureSensor(int intervall, uint8_t address, Bcm2835Interface* wire) : Sensor(intervall)
